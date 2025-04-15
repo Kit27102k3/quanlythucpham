87971c7c7a087d0100a5f6b581b44dee1fa0b468
Trọng Khiêm <kit10012003@gmail.com>
2025-04-15 10:34:33 +0700

    Cài đặt gói serverLess-http

--- Server/api/index.js
+++ Server/api/index.js
@@ -1,9 +1,11 @@
-// api/index.js
-const express = require('express');
+import express from "express";
+import serverless from "serverless-http";
+
 const app = express();
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from Vercel Express' });
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Vercel Express" });
 });
 
-module.exports = app;
+// Export theo đúng chuẩn Vercel cần
+export default serverless(app);
