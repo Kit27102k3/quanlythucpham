00b82c9a7df1eea685036105baf45d9b17779e13
Trọng Khiêm <kit10012003@gmail.com>
2025-04-15 10:52:07 +0700

    Fix lỗi vercel

--- Server/api/index.js
+++ Server/api/index.js
@@ -1,13 +1,11 @@
+// api/index.js
 import express from "express";
 import serverless from "serverless-http";
 
 const app = express();
-const router = express.Router();
 
-router.get("/hello", (req, res) => {
-  res.json({ message: "Hello from Vercel Express" });
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Vercel Express!" });
 });
-
-app.use("/api", router);
 
 export default serverless(app);
