9483f9c4a6017cb036e43f23b0300751a56247ec
Trọng Khiêm <kit10012003@gmail.com>
2025-06-16 00:28:32 +0700

    Fix deploy nữa nè file nưa đó nhe

---
+++ Server/.babelrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  presets: ['env'],
+  plugins: [
+    'transform-object-rest-spread',
+    'dynamic-import-node',
+    'transform-es2015-modules-commonjs',
+    'transform-async-to-generator',
+    'transform-class-properties',
+    'transform-runtime'
+  ]
+}; 
